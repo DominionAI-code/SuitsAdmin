03b82d02bc2f2f6690d09d76c6814fe4244ed9d9
Dominion A <dominionai394@gmail.com>
2025-03-03 10:07:31 -0800

    Change component folder's name and context

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -1,7 +1,7 @@
 // src/pages/TableManagementPage.jsx
 import { useContext } from 'react';
-import { TableContext } from '../context/TableContext';
-import { ReservationContext } from '../context/ReservationContext';
+import { TableContext } from '../Context/TableContext';
+import { ReservationContext } from '../Context/ReservationContext';
 import TableList from './TableList';
 import CreateTableForm from './CreateTableForm';
 import ReservationList from './ReservationList';
